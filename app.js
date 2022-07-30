279b84e027ad71fabcb8923c094d498a8f2bcc23
Valentyn Kovalchuk <kovalhood@gmail.com>
2022-07-30 10:51:46 +0300

    HW-3 Finished

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+global.basedir = __dirname;
 
 const contactsRouter = require('./routes/api/contacts')
 
@@ -22,4 +27,4 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: err.message })
 })
 
-module.exports = app
+module.exports = app;
